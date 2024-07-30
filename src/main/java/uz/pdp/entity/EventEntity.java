3a0fe1910bebddf078c63b58a29a0e605d273446
Xusanboy <xusanboy@gmail.com>
2024-07-30 15:32:59 +0500

    first commit

---
+++ src/main/java/uz/pdp/entity/EventEntity.java
@@ -0,0 +1,31 @@
+package uz.pdp.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import uz.pdp.entity.type.EventType;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "events")
+public class EventEntity extends BaseEntity{
+    private EventType type;
+    private String locationName;
+    private Double locationPrice;
+    private Integer capacity;
+    private Double ticketPrice;
+    private Integer availableSeats;
+    private String picture;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    @OneToMany(mappedBy = "event", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, fetch = FetchType.LAZY)
+    private List<TicketEntity> tickets;
+
+}
---
+++ src/main/java/uz/pdp/entity/type/EventType.java
@@ -0,0 +1,5 @@
+package uz.pdp.entity.type;
+
+public enum EventType {
+    CONCERT, SHOU, GAME, MOVIE
+}
